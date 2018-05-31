e4cb39d3a3240c857deb83c9eeb650b3ff592dc0
Amiya <amiyasrvstv@gmail.com>
2018-05-31 12:00:33 +0530

    added login page

--- node_rest_api_mysql_usage/server.js
+++ node_rest_api_mysql_usage/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.get('/', (req, res) => res.render('index'));
+app.use(express.static(path.join(__dirname, "/public")));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname + '/public/login.html')));
 app.listen(4000, () => console.log('The node_rest_api_mysql_usage server is live at port 4000'));
